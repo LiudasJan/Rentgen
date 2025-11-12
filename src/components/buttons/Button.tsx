ed3d67ed061ac2476e0817642a1d1fd1007d0151
Andrej Davidovič <andav1992@gmail.com>
2025-11-12 12:03:56 +0200

    Small fix

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -25,9 +25,8 @@ export default function Button({
         {
           'bg-button-primary border border-button-primary text-white': buttonType === ButtonType.PRIMARY,
           'hover:bg-button-primary/80 hover:border-button-primary/80': buttonType === ButtonType.PRIMARY && !disabled,
-          'bg-button-secondary border border-button-secondary': buttonType === ButtonType.SECONDARY,
-          'hover:bg-button-secondary/80 hover:border-button-secondary/80':
-            buttonType === ButtonType.SECONDARY && !disabled,
+          'bg-button-secondary border border-border': buttonType === ButtonType.SECONDARY,
+          'hover:bg-button-secondary/80 hover:border-border/80': buttonType === ButtonType.SECONDARY && !disabled,
           'opacity-50 cursor-not-allowed': disabled,
           'cursor-pointer': !disabled,
         },
