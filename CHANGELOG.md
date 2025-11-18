2363861541832655bda7d2f75663d212001da142
Donatas Puidokas <104410044+donpui@users.noreply.github.com>
2025-11-18 20:15:14 +0200

    added github actions and building for arm64 abd x64
    adjust version

---
+++ CHANGELOG.md
@@ -0,0 +1,39 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [v1.2.1] - 2025-11-19
+
+### Changed
+
+- Build for x64, arm64 arch: mac, win, linux
+
+### Added
+
+- Added Github actions for build and release
+
+### Fixed
+
+- Fixed [#10](https://github.com/LiudasJan/Rentgen/issues/10)
+
+### Note
+
+💻 How to Run on macOS
+
+macOS may block the app (“developer cannot be verified”).
+To run it normally:
+
+1. Move Rentgen.app to the Applications folder.
+2. Open Terminal and run the following command:
+
+`xattr -d com.apple.quarantine /Applications/Rentgen.app`
+
+After this, you can launch Rentgen from Finder or Spotlight as usual.
+
+[Unreleased]: https://github.com/LiudasJan/Rentgen/compare/v1.2.0...HEAD
+[1.2.0]: https://github.com/LiudasJan/Rentgen/releases/tag/v1.2.0
--- forge.config.ts
+++ forge.config.ts
@@ -1,6 +1,7 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
@@ -14,9 +15,23 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    executableName: 'Rentgen',
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerDMG({}),
+    new MakerRpm({
+      options: {
+        bin: 'Rentgen',
+      },
+    }),
+    new MakerDeb({
+      options: {
+        bin: 'Rentgen',
+      },
+    }),
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
