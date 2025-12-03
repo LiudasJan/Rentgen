879314a4c616b7afc31da5afb578e16a614abbdc
Andrej Davidovič <andav1992@gmail.com>
2025-12-03 12:26:33 +0200

    Updated reload text

--- src/App.tsx
+++ src/App.tsx
@@ -242,7 +242,7 @@ export default function App() {
             <Modal className="[&>div]:w-[400px]!" isOpen={openReloadModal} onClose={closeReloadModal}>
               <div className="flex flex-col gap-4">
                 <h4 className="m-0">Reload</h4>
-                <p className="m-0 text-sm dark:text-text-secondary">All current data will be lost</p>
+                <p className="m-0 text-sm dark:text-text-secondary">Only current tests results will be lost</p>
                 <div className="flex items-center justify-end gap-4">
                   <Button buttonType={ButtonType.DANGER} onClick={() => window.location.reload()}>
                     Reload
